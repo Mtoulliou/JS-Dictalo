514b8fc64fa5fe9f7f280c4b681510d8c7d5df2a
TOULLIOU Matteo (EQUANS FR) <hk8205@equans.com>
2025-02-09 20:39:10 +0100

    Finish text generation and initialization of the game

--- script.js
+++ script.js
@@ -1,35 +1,11 @@
-const words = {
-    "easy": [
-        "le", "de", "un", "être", "et", "à", "il", "avoir", "ne", "je",
-        "son", "que", "se", "qui", "ce", "dans", "en", "du", "elle", "au",
-        "avec", "pour", "pas", "vous", "par", "sur", "faire", "plus", "dire", "me",
-        "on", "mon", "lui", "nous", "comme", "mais", "ils", "ou", "si", "leur",
-        "tout", "y", "bien", "où", "peu", "voir", "quelque", "sans", "autre", "quand",
-        "très", "même", "prendre", "aussi", "donner", "temps", "ses", "notre", "dont", "rien",
-        "alors", "moi", "être", "savoir", "aller", "avant", "devoir", "venir", "pendant", "juste",
-        "contre", "après", "personne", "dernier", "toujours", "certain", "aucun", "grand", "petit", "jour",
-        "femme", "homme", "nom", "grand", "place", "gens", "enfant", "chose", "voix", "pied",
-        "oeil", "tête", "bras", "visage", "coeur", "main", "noir", "blanc", "bleu", "rouge",
-        "long", "court", "nouveau", "vieux", "premier", "dernier", "mieux", "mal", "fort", "faible",
-        "heure", "minute", "semaine", "mois", "année", "hier", "aujourd'hui", "demain", "matin", "soir",
-        "nuit", "heureux", "triste", "jeune", "vieux", "riche", "pauvre", "chaud", "froid", "sec",
-        "humide", "faim", "soif", "malade", "fatigué", "content", "calme", "nerveux", "facile", "difficile",
-        "possible", "impossible", "vrai", "faux", "intéressant", "ennuyeux", "important", "inutile", "beau", "laid",
-        "gros", "mince", "rapide", "lent", "léger", "lourd", "proche", "loin", "propre", "sale",
-        "jeune", "ancien", "fort", "bruyant", "silencieux", "vide", "plein", "haut", "bas", "large",
-        "étroit", "court", "long", "dur", "mou", "sucré", "amer", "salé", "acide", "bon",
-        "mauvais", "cher", "pas cher", "chaud", "froid", "humide", "sec", "neuf", "usé", "vivant",
-        "mort", "facile", "difficile", "possible", "impossible", "intéressant", "ennuyeux", "heureux", "triste", "riche",
-        "pauvre", "sympathique", "antipathique", "gentil", "méchant", "intelligent", "bête", "drôle", "sérieux", "fatigué",
-        "éveillé", "endormi", "occupé", "libre", "pressé", "lent", "rapide", "fort", "faible", "bruyant",
-        "silencieux", "propre", "sale", "humide", "sec", "chaud", "froid", "bon", "mauvais", "grand",
-        "petit", "large", "étroit", "gros", "mince", "épais", "fin", "dur", "mou", "sucré",
-        "amer", "salé", "acide", "piquant", "fade", "léger", "lourd", "clair", "foncé", "lumineux",
-        "sombre", "heure", "minute", "seconde", "jour", "semaine", "mois", "année", "printemps", "été", "automne",
-        "hiver", "matin", "midi", "après-midi", "soir", "nuit", "hier", "aujourd'hui", "demain", "lundi",
-        "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche", "instant", "moment", "début", "fin", "tôt",
-    ]
+async function getWords() {
+    // get the list of words from the json file
+    const response = await fetch('words.json');
+    const data = await response.json();
+    console.log(data.words);
+    return data.words;
 }
+
 function getRandomInt(max) {
     // get random number between 0 and max
     return Math.floor(Math.random() * max);
@@ -38,22 +14,52 @@ function getRandomInt(max) {
 function generateTexte(words) {
     // generate a list of text with 100 words from the words list
     let texte = [];
-    const max = words.easy.length;
+    const max = words.length;
     for (let i = 0; i < 100; i++) {
-        texte.push(words.easy[getRandomInt(max)]);
+        texte.push(words[getRandomInt(max)]);
     }
     console.log(texte);
     return texte;
 }
 
-async function startGame() {
+async function initGame() {
     // init the game for ready to start
-    //const words = await fetch("./words.json")
-    //    .then(response => response.json())
-    //    .then(data => data.words);
-    console.log(words.easy[0]);
+
+    // get the list of words
+    const words = await getWords();
+
+    // remove the start button
     const startButton = document.getElementById('startButton');
-    startButton.remove();
+    startButton.innerText = 'Restart';
+
+    // remove the previous text and input
+    let oldTexte = document.getElementById('texte');
+    if (oldTexte) {
+        oldTexte.remove();
+    }
+    let oldInput = document.getElementById('input');
+    if (oldInput) {
+        oldInput.remove();
+    }
+
+    // generate the text
     let texte = document.createElement('p');
-    texte.innerText = generateTexte(words);
+    texte.id = 'texte';
+    texteGenerated = generateTexte(words);
+    for (let i = 0; i < 100; i++) {
+        texte.innerText += texteGenerated[i] + " ";
+    }
+    document.body.appendChild(texte);
+
+    let input = document.createElement('input');
+    input.id = 'input';
+    input.type = 'text';
+    input.placeholder = 'Start typing...';
+    input.addEventListener('input', startGame);
+    document.body.appendChild(input);
+}
+
+function startGame() {
+    // start the game
+    console.log('Game started');
 }
